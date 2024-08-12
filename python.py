6c40566592d504b3307cd61771d95536c4d092b5
GuilhermeKendi <falecomkendiguilherme@gmail.com>
2024-08-12 20:23:11 -0300

    primeiros testes no python

---
+++ python.py
@@ -0,0 +1,21 @@
+
+#TESTE DE ENTRADAS E SAIDAS UTILIZANDO TUPLAS 
+entrada = {
+    "Categoria": "null",
+    "Valor": 0
+    }
+
+entrada['Categoria'] = input('Digite o nome da categoria')
+entrada['Valor'] = float(input('Digite um valor: '))
+
+print(f'Primeiro registro entrada: {entrada}')
+
+saida = {
+    "Categoria": "null",
+    "Valor": 0
+    }
+
+saida['Categoria'] = input('Digite o nome da categoria')
+saida['Valor'] = float(input('Digite um valor: '))
+
+print(f'Primeiro registro de saida: {saida}')
